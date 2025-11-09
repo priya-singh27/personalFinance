e53128ffee525e2156977ce06dd65a6efbf71359
priya-singh27 <priya.beingdev@gmail.com>
2025-11-09 22:10:53 +0530

    add db port

--- backend/db_config.js
+++ backend/db_config.js
@@ -14,6 +14,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
